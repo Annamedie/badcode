a4a84a3076690f39334a6a8c84e73518f074926f
annaa <annaalehed@hotmail.com>
2024-04-03 19:52:04 +0200

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [isGreen, setIsGreen] = useState(false);
 
   function toggleBackground() {
+    alert("You Changed the color");
     setIsGreen(!isGreen);
   }
 
@@ -19,6 +20,7 @@ function App() {
     setTitle(event.target.value);
   }
   function addTask() {
+    alert("You added something");
     if (title.trim() !== "") {
       setTask([title, ...tasks]);
       setTitle("");
@@ -26,15 +28,24 @@ function App() {
   }
 
   function deleteTask(index) {
+    alert("You removed something");
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTask(updatedTasks);
   }
+  function doCount() {
+    prompt("You added a count");
+    setCount((count) => count + 1);
+  }
 
   return (
-    <div style={greenContainer}>
-      <div>
-        <h1>Att göra lista</h1>
+    <div
+      style={greenContainer}
+      className="flex flex-col items-center justify-center gap-4 min-h-screen p-8"
+    >
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="text-2xl font-bold text-gray-800">Att göra lista</h1>
         <input
+          className="border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
           type="text"
           placeholder="Skriv ngt att göra"
           value={title}
@@ -47,12 +58,15 @@ function App() {
           Add
         </button>
       </div>
-      <div>
+      <div className="flex flex-col gap-2">
         {tasks.map((task, index) => (
-          <div key={index}>
+          <div
+            className="flex justify-between items-center border border-gray-200 rounded-lg p-2"
+            key={index}
+          >
             <div>{task}</div>
             <button
-              className="bg-lime-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="bg-lime-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
               onClick={() => deleteTask(index)}
             >
               delete
@@ -62,7 +76,7 @@ function App() {
       </div>
       <button
         className="bg-gray-800 text-white font-semibold py-2 px-4 rounded transform hover:scale-105 hover:bg-gray-700 hover:shadow-lg transition duration-300 ease-in-out"
-        onClick={() => setCount((count) => count + 1)}
+        onClick={doCount}
       >
         count is {count}
       </button>
